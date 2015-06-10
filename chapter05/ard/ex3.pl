8ed8f16a73d443213891f0102a65fec51efa7ef2
Andrew Davey <ard@datacash.com>
2015-06-10 14:09:57 +0100

    Removed 'any host' hack from ex3, since it's not needed

--- chapter05/ard/ex3.pl
+++ chapter05/ard/ex3.pl
@@ -7,15 +7,10 @@ use 5.010;
 
 my %data = ();
 
-# Bit of a hack - I'm using this as a fake "destination" host to count total traffic
-# from each source. Contains asterisks so it shouldn't clash with a real host name.
-my $any_dest = "*ANYWHERE*";
-
 while ( my $line = <> ) {
   next if ( $line =~ m/^\s*#/ );
   my ( $source, $dest, $bytes ) = split( /\s+/, $line );
   $data{$source}{$dest} += $bytes;
-  $data{$source}{$any_dest} += $bytes;
 }
 
 # We need a list of source hosts, sorted by hostname
@@ -24,7 +19,6 @@ foreach my $source ( sort keys %data ) {
 
   # And now a list of destination hosts sorted by hostname 
   foreach my $dest ( sort keys %{ $data{$source} } ) {
-    next if ( $dest eq $any_dest );
     # The tabs are just for Alex - he loves them!
     printf( "\t%s\t%u\n", $dest, $data{$source}{$dest} );
   }
