a47fbdbee8fd4f43cb0017619688ffcd2ed5ee0c
ardavey <ardavey@gmail.com>
2015-07-10 00:23:31 +0100

    Finished ex1

--- chapter08/ard/ex1.pl
+++ chapter08/ard/ex1.pl
@@ -3,33 +3,65 @@ use strict;
 use warnings;
 use 5.010;
 
+# use all of the IO modules!
 use Time::Piece;
 use IO::Handle;
 use IO::Scalar;
 use IO::Tee;
 
+# nasty globals, but will do for now
+my $output_handle; # this will ultimately point to our destination(s)
+my $scalar;
+
 my $t = localtime;
-my $output = sprintf( "Today is %s %u %s %u", $t->fullday, $t->day_of_month, $t->fullmonth, $t->year );
+my $output_string = sprintf( "Today is %s %u %s %u", $t->fullday, $t->day_of_month, $t->fullmonth, $t->year );
 
-my $file_fh;
-my $scalar_fh;
 
-print "Output to File, Scalar or Both? [FSB] ";
-my $choice = <STDIN>;
+print "Output to file, scalar or both? [fsb] ";
+chomp( my $choice = lc( <STDIN> ) );
+#$choice = lc( $choice );
 
-if ( lc( $choice ) eq 'f' ) {
-  say "Writing to file $0.$$.out";
-  $file_fh = IO::File->new( "$0.$$.out", '>' ) or die "Failed to open output file: $!";
+my %dispatch = (
+  f => \&do_file,
+  s => \&do_scalar,
+  b => \&do_both,
+);
+
+( $dispatch{$choice} || \&do_invalid )->(); 
+
+print $output_handle $output_string;
+say STDOUT '$scalar contains:';
+say STDOUT defined $scalar ? $scalar : "(nothing)";  
+
+
+sub do_file {
+  say STDOUT "Writing to file $0.$$.out";
+  $output_handle = get_file_handle();
 }
-elsif ( lc( $choice ) eq 's' ) {
-  say "Writing to scalar";
-  $scalar_fh = IO::Scalar->new();
-}
-elsif ( lc( $choice ) eq 'b' ) {
 
+sub do_scalar {
+  say STDOUT "Writing to scalar";
+  $output_handle = get_scalar_handle();  
 }
-else {
+
+sub do_both {
+  say STDOUT "Writing to file and scalar";
+  my $fh = get_file_handle();
+  my $sh = get_scalar_handle();
+  $output_handle = IO::Tee->new( $fh, $sh ) or die "Failed to create tee handle: $!";
+}
+
+sub do_invalid {
   say "Unrecognised input - must be F,S or B.";
   exit 1;
 }
 
+sub get_file_handle {
+  my $handle = IO::File->new( "$0.$$.out", '>' ) or die "Failed to open file handle: $!";
+  return $handle;
+}
+
+sub get_scalar_handle {
+  my $handle = IO::Scalar->new( \$scalar, '>' ) or die "Failed to open scalar handle: $!";
+  return $handle;
+}
