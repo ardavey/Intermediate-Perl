63309710715a4982387255a08705eccc8799a57f
Simon Gross <spg@datacash.com>
2015-08-13 09:11:40 +0100

    Finished

--- spg/Animal/bin/barnyard.pl
+++ spg/Animal/bin/barnyard.pl
@@ -1,8 +1,27 @@
-#! /usr/bin/perl
+#!/usr/bin/perl
 
 use strict;
 use warnings;
 
 use Animal;
+use Cow;
+use Horse;
+use Sheep;
+use Mouse;
 
-Animal->speak();
+print "Build your own farm!\n";
+
+my @barnyard
+
+while ( my $input = <STDIN> ) {
+  chomp( $input ); 
+  push @barnyard, $input;
+}
+
+foreach my $animal ( @barnyard ) {
+  eval { $animal->speak(); };
+
+  if ( $@ ) {
+    print "$animal is not an animal we are allowed on this farm...\n";
+  } 
+}
