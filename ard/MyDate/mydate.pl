3c3926c3acf8a6418590cc2adc5cb4249e95c8be
Andrew Davey <ard@datacash.com>
2015-09-03 00:54:36 +0100

    Added ch16

---
+++ ard/MyDate/mydate.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use 5.010;
+
+use MyDate;
+
+use Data::Dumper;
+use Time::Piece;
+
+my $d = new MyDate;
+
+sub UNIVERSAL::debug {
+  my ( $self, $msg ) = @_;
+  my $t = localtime;
+  print "$t: $msg\n";
+}
+
+foreach my $thing ( qw( date month year ) ) {
+  say "$thing: " . $d->$thing();
+}
+
+# This isn't picked up by AUTOLOAD because UNIVERSAL is checked first
+$d->debug( "Time to go to bed!" );
+
+say "blarg: " . $d->blarg();
+
